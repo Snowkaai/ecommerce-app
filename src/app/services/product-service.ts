4bf126e14924e0803e342d745d12fa9d387588a2
marwa samy <marwa.elfayoumy9@gmail.com>
2026-05-13 16:35:35 +0300

    get products

--- src/app/services/product-service.ts
+++ src/app/services/product-service.ts
@@ -1,6 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Product } from '../Models/IProduct';
+import { baseURL } from '../Models/api';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {}
+export class ProductService {
+  http= inject(HttpClient);
+  products= signal<Product[]>([]);
+
+  GetAllProducts(){
+  return this.http.get<any[]>(baseURL+'/products').subscribe({
+    next:(data)=>{
+     const mappedData:Product[] = data.map((prod)=>({
+      id:prod.id,
+      title:prod.title,
+      price:prod.price,
+      category:prod.category
+     }));
+
+     this.products.set(mappedData);
+     console.log(this.products());
+        },
+    error:(e)=>{ console.error(e); }
+  });
+}
+
+}
--- src/app/services/stripe.ts
+++ src/app/services/stripe.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { loadStripe } from '@stripe/stripe-js';
+//import { loadStripe } from '@stripe/stripe-js';
 import { environment } from '../../environments/environment';
 
 @Injectable({
