fc1da4109e926c503118542b9ae7a7aae9a4e322
Andrew Hany Sobhy <andrewhfarid@gmail.com>
2026-05-13 15:25:50 +0300

    Added Initial Routing, Header component, home component, and notfound components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { Notfound } from './components/notfound/notfound';
+import { Header } from './components/header/header';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', //Empty path will redirect to main
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  {
+    path: 'main',
+    component: Header,
+    children: [
+      {
+        path: '', //Empty path will redirect to home
+        redirectTo: 'Home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'Home',
+        component: Home,
+      },
+      {
+        path: '**',
+        component: Notfound,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: Notfound,
+  },
+];
