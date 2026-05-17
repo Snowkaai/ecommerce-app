48031f0db32bb2ba6fbcf86cad854c0c16ddb07d
Andrew Hany Sobhy <andrewhfarid@gmail.com>
2026-05-18 00:41:09 +0300

    added editor's choice menu

--- src/app/Models/User.ts
+++ src/app/Models/User.ts
@@ -7,7 +7,7 @@
 //   "Wishlist" : int[] ==>products
 //   "Orders" : DoneOrder[] => {cartitem[] , total , date}
 
-import { CartItem, Product } from './IProduct';
+import { CartItem } from './ICartItem';
 
 export interface appuser {
   id: string;
