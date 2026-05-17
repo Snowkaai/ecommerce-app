b479a5540f47e38ccee5c0862b1c9533ccec2730
Andrew Hany Sobhy <andrewhfarid@gmail.com>
2026-05-18 00:41:09 +0300

    added editor's choice menu

--- src/app/components/productcard/productcard.ts
+++ src/app/components/productcard/productcard.ts
@@ -14,6 +14,7 @@ import { Notfound } from '../../pages/notfound/notfound';
 })
 export class Productcard {
   @Input() product!: Product;
+  @Input() ShopVerseChoice: boolean = false;
 
   currentIndex = signal(0);
   isFading = signal(false);
