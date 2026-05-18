364c7f6f98feae28cabcb299c8fceb16db56a9f0
Mohamed Walid <mohamedzeidan3377@gmail.com>
2026-05-18 11:41:33 +0300

    Made order-history component & profile-page
    separated profile & order-history into separated components and added this to new profile-page.

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { Authservice } from '../../../services/authservice';
+import { Authservice } from '../../services/authservice';
 
 
 @Component({
