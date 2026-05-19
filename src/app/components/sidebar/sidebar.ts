8d7796b8b98cada3ec06ca7fc250bc9858005565
Andrew Hany Sobhy <andrewhfarid@gmail.com>
2026-05-19 18:51:47 +0300

    Improved styling for product card, adjusted notification height, finished sidebar

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -2,23 +2,24 @@ import { Component, inject } from '@angular/core';
 import { ProductService } from '../../services/product-service';
 import { WishlistService } from '../../services/wishlist-service';
 import { Authservice } from '../../services/authservice';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  wishlistService = inject(WishlistService);
+  user = inject(Authservice);
 
-  wishlistService=inject(WishlistService);
-  user=inject(Authservice);
-  
-getWishlist() {
-   const currentUser = this.user.currentUser();
-  if (currentUser){
-  this.wishlistService.GetWishlist(currentUser);
+  getWishlist() {
+    const currentUser = this.user.currentUser();
+    if (currentUser) {
+      this.wishlistService.GetWishlist(currentUser);
+    }
   }
-}
 
   productserv = inject(ProductService);
 
